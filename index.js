24bdfff701507369749e9d9909fc1855ad297fa9
mahadi40011 <mahadi40011@gmail.com>
2025-11-11 10:16:37 +0600

    Implement get(one data) method and  successfully get one data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -36,9 +36,16 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/transaction", async (req, res) => {
+    app.get("/transactions", async (req, res) => {
       const cursor = TransactionCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/transaction/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await TransactionCollection.findOne(query);
       res.send(result);
     });
 
